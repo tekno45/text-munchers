ea67d3235b84d5870cd4a1c303a8091acdbd4c73
Joseph Greene <josephgreene78@gmail.com>
2016-03-22 23:05:38 -0700

    made one method for taking string and awarding points
    trying to add words to bonus list by csv, having issues making it work.

--- TextInput.py
+++ TextInput.py
@@ -1,34 +1,44 @@
-
+import os
+import csv
 #Aaron hall, fastest way to find a string inside a string
 def checkwrds(wrds,stuffs):
     return set(wrds).intersection(stuffs.split())
 
 #Take in text and award points
-def points4words (text,bonus):
+def points4words (text, bonus):
+    bonusinstuff = [text.count(word) for word in bonus]
+    points = len(text)
+    message = "Your text is worth {} points!"
+    print(message.format(int(points)))
+    bonuspoints = 0
+    for x in bonusinstuff:
+        if x >= 1:
+            bonuspoints = bonuspoints +1
+    if bonuspoints > 0:
+        bonusmessage  = "WOAH you earned {} bonus points!"
+        print(bonusmessage.format(bonuspoints))
+
+os.chdir("d:/and")
+x = open("data.csv")
+bonuslist = ["roar", "dumb", "eat"]
+for row in csv.reader(x):
+    bonuslist.append(str(row[0].lower))
+
+points4words(input("Enter words: "), bonuslist)
 
 
-stuff = input("Enter words: ")
-bonuswords = ["butts", "stuff"]
-bonuspoints = 0
+#print (bonusinstuff)
+#print(bonuspoints)
+#stuff = input("Enter words: ")
+#bonuswords = ["butts", "stuff"]
+#bonuspoints = 0
 
-#for word in checkwrds(bonuswords,stuff):
- #   bonuspoints = bonuspoints +1
 
-bonusinstuff  = [stuff.count(word) for word in bonuswords]
 
-for x in bonusinstuff:
-    if x >= 1:
-        bonuspoints = bonuspoints +1
-points = len(stuff)
-message = "Your text is worth {} points!"
-print(message.format(int(points)))
 
-if bonuspoints > 0:
-    bonusmessage  = "WOAH you earned {} bonus points!"
-    print(bonusmessage.format(bonuspoints))
 
-print (bonusinstuff)
-print(bonuspoints)
+
+
 
 
 
