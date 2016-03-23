de6ea36664ad3435d0d46f5562f57233c17baddd
Joseph Greene <josephgreene78@gmail.com>
2016-03-22 21:00:42 -0700

    TextInput handles taking in the input and awarding points
    bonus points are awarded if you enter words on the bonuslist
    
    Text and points may be split later if these both get complicated enough.

--- TextInput.py
+++ TextInput.py
@@ -1,19 +1,35 @@
 
-
+#Aaron hall, fastest way to find a string inside a string
 def checkwrds(wrds,stuffs):
     return set(wrds).intersection(stuffs.split())
 
 #Take in text and award points
+def points4words ():
 
 stuff = input("Enter words: ")
 bonuswords = ["butts", "stuff"]
 bonuspoints = 0
-for word in checkwrds(bonuswords,stuff):
-    bonuspoints = bonuspoints +1
-points = len(stuff) + bonuspoints
 
+#for word in checkwrds(bonuswords,stuff):
+ #   bonuspoints = bonuspoints +1
+
+bonusinstuff  = [stuff.count(word) for word in bonuswords]
+
+for x in bonusinstuff:
+    if x >= 1:
+        bonuspoints = bonuspoints +1
+points = len(stuff)
 message = "Your text is worth {} points!"
-
 print(message.format(int(points)))
+
+if bonuspoints > 0:
+    bonusmessage  = "WOAH you earned {} bonus points!"
+    print(bonusmessage.format(bonuspoints))
+
+print (bonusinstuff)
+print(bonuspoints)
+
+
+
 
 
